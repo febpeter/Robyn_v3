c315bba08c0df26060cb4b112840d91e4cb8b2b0
Soham <97466399+idoncode@users.noreply.github.com>
2024-02-22 14:27:37 +0530

    Update model.R

--- R/R/model.R
+++ R/R/model.R
@@ -714,7 +714,7 @@ robyn_mmm <- function(InputCollect,
             # lambda_max <- max(lambdas)
             lambda_hp <- unlist(hypParamSamNG$lambda[i])
             if (hyper_fixed == FALSE) {
-              lambda_scaled <- lambda_min + (lambda_max - lambda_min) * lambda_hp
+              lambda_scaled <- 0
             } else {
               lambda_scaled <- lambda_hp
             }
