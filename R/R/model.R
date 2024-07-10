fc3827249617c2b4c00d7ec22be256911df2caa7
febpeter <85392489+febpeter@users.noreply.github.com>
2024-07-10 18:06:45 +0530

    Update model.R

--- R/R/model.R
+++ R/R/model.R
@@ -936,7 +936,7 @@ robyn_mmm <- function(InputCollect,
           if (!hyper_fixed) {
             if (is.null(calibration_input)) {
               for (co in 1:iterPar) {
-                optimizer$tell(nevergrad_hp[[co]], tuple(nrmse.collect[co], MAPE.collect[co]))
+                optimizer$tell(nevergrad_hp[[co]], tuple(decomp.rssd.collect[co], MAPE.collect[co]))
               }
             } else {
               for (co in 1:iterPar) {
